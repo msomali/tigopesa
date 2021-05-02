c3dd87290d9a7ebb8a7c7e8c86c0bf2fe4ecab90
Pius Alfred <me.pius1102@gmail.com>
2021-05-02 14:14:30 +0300

    ussd

--- ussd/service.go
+++ ussd/service.go
@@ -5,6 +5,16 @@ import (
 	"encoding/xml"
 )
 
+//type TxnStatus map[string]string
+//
+//var (
+//	Statuses = TxnStatus{
+//		"200":"Success",
+//		"0":"Success",
+//		"410": "unable to complete transaction, amount more than maximum limit",
+//	}
+//)
+
 type NameCheckErrorCode string
 
 const (
@@ -12,6 +22,22 @@ const (
 	NOT_REGISTERED NameCheckErrorCode = "error010"
 	SUSPENDED      NameCheckErrorCode = "error020"
 	INVALID_FORMAT NameCheckErrorCode = "error030"
+)
+
+type W2AErrorCode string
+
+const (
+	SUCCESSFUL_TRANSACTION      W2AErrorCode = "error000"
+	SERVICE_NOT_AVAILABLE       W2AErrorCode = "error001"
+	INVALID_CUSTOMER_REF_NUMBER W2AErrorCode = "error010"
+	CUSTOMER_REF_NUM_LOCKED     W2AErrorCode = "error011"
+	INVALID_AMOUNT              W2AErrorCode = "error012"
+	AMOUNT_INSUFFICIENT         W2AErrorCode = "error013"
+	AMOUNT_TOO_HIGH             W2AErrorCode = "error014"
+	AMOUNT_TOO_LOW              W2AErrorCode = "error015"
+	INVALID_PAYMENT             W2AErrorCode = "error016"
+	GENERAL_ERROR               W2AErrorCode = "error100"
+	RETRY_CONDITION_NO_RESPONSE W2AErrorCode = "error111"
 )
 
 type QuerySubscriberNameReq struct {
@@ -35,20 +61,64 @@ type QuerySubscriberNameResp struct {
 	Content   string   `xml:"CONTENT"`
 }
 
+// WalletToAccountReq Input to the Partner Application from the Payment Gateway – SYNC_BILL
+// PAY_API Request
+// TYPE The Request Type of Transaction, the value will be constant in all request
+// it will be SYNC_BILLPAY_REQUEST
+// TXID Alphanumeric this filed will be Tigo Transaction ID, unique for all Transactions
+// MSISDN Payer MSISDN it will be without country code
+// AMOUNT amount in the transaction
+// COMPANYNAME biller code or Business Number
+// CUSTOMERREFERENCEID BillPay reference number normally generated/shared by partner application to validate payments
+// SENDERNAME Name of Sender or Payer (50)
 type WalletToAccountReq struct {
-
+	XMLName             xml.Name `xml:"COMMAND"`
+	Text                string   `xml:",chardata"`
+	TYPE                string   `xml:"TYPE"`
+	TXID                string   `xml:"TXID"`
+	MSISDN              string   `xml:"MSISDN"`
+	AMOUNT              string   `xml:"AMOUNT"`
+	COMPANYNAME         string   `xml:"COMPANYNAME"`
+	CUSTOMERREFERENCEID string   `xml:"CUSTOMERREFERENCEID"`
+	SENDERNAME          string   `xml:"SENDERNAME"`
 }
 
 type WalletToAccountResp struct {
-
+	XMLName          xml.Name `xml:"COMMAND"`
+	Text             string   `xml:",chardata"`
+	TYPE             string   `xml:"TYPE"`
+	TXID             string   `xml:"TXID"`
+	REFID            string   `xml:"REFID"`
+	RESULT           string   `xml:"RESULT"`
+	ERRORCODE        string   `xml:"ERRORCODE"`
+	ERRORDESCRIPTION string   `xml:"ERRORDESCRIPTION"`
+	MSISDN           string   `xml:"MSISDN"`
+	FLAG             string   `xml:"FLAG"`
+	CONTENT          string   `xml:"CONTENT"`
 }
 
 type AccountToWalletReq struct {
-
+	XMLName     xml.Name `xml:"COMMAND"`
+	Text        string   `xml:",chardata"`
+	TYPE        string   `xml:"TYPE"`
+	REFERENCEID string   `xml:"REFERENCEID"`
+	MSISDN      string   `xml:"MSISDN"`
+	PIN         string   `xml:"PIN"`
+	MSISDN1     string   `xml:"MSISDN1"`
+	AMOUNT      string   `xml:"AMOUNT"`
+	SENDERNAME  string   `xml:"SENDERNAME"`
+	LANGUAGE1   string   `xml:"LANGUAGE1"`
+	BRANDID     string   `xml:"BRAND_ID"`
 }
 
 type AccountToWalletResp struct {
-
+	XMLName     xml.Name `xml:"COMMAND"`
+	Text        string   `xml:",chardata"`
+	TYPE        string   `xml:"TYPE"`
+	REFERENCEID string   `xml:"REFERENCEID"`
+	TXNID                string   `xml:"TXNID"`
+	TXNSTATUS string `xml:"TXNSTATUS"`
+	MESSAGE string `xml:"MESSAGE"`
 }
 
 type Service interface {
@@ -65,7 +135,7 @@ type Service interface {
 	//</COMMAND>
 	QuerySubscriberName(ctx context.Context, req QuerySubscriberNameReq) (resp QuerySubscriberNameResp, err error)
 
-	WalletToAccount(ctx context.Context, req WalletToAccountReq)(resp WalletToAccountResp, err error)
+	WalletToAccount(ctx context.Context, req WalletToAccountReq) (resp WalletToAccountResp, err error)
 
-	AccountToWallet(ctx context.Context, req AccountToWalletReq)(resp AccountToWalletResp, err error)
+	AccountToWallet(ctx context.Context, req AccountToWalletReq) (resp AccountToWalletResp, err error)
 }

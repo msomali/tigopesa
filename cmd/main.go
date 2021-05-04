1408a7104bec34fde10e088553cc508123ec7b0d
Pius Alfred <me.pius1102@gmail.com>
2021-05-04 10:20:10 +0300

    w2a

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"encoding/xml"
 	"fmt"
 	"github.com/techcraftt/tigosdk"
@@ -9,6 +11,7 @@ import (
 	"github.com/techcraftt/tigosdk/ussd"
 	"io/ioutil"
 	"net/http"
+	"strconv"
 	"time"
 )
 import "github.com/gorilla/mux"
@@ -30,11 +33,19 @@ func MakeHandler(svc tigosdk.Service) http.Handler {
 
 	router.HandleFunc("/api/tigopesa/c2b/transactions", app.transactionHandler).Methods(http.MethodPost, http.MethodGet)
 
+	router.HandleFunc("/api/tigopesa/disburse", app.disburseHandler).Methods(http.MethodPost)
+
 	return router
+}
+
+type disburseInfo struct {
+	Msisdn string `json:"msisdn"`
+	Amount float64 `json:"amount"`
 }
 
 type App struct {
 	svc tigosdk.Service
+	conf tigosdk.Configs
 }
 
 func (app App) transactionHandler(w http.ResponseWriter, request *http.Request) {
@@ -91,8 +102,9 @@ func (app App) namesHandler(w http.ResponseWriter, request *http.Request) {
 		return
 	}
 
-	resp, err2 := app.svc.QuerySubscriberName(context.TODO(), req)
-	if err2 != nil {
+	resp, err := app.svc.QuerySubscriberName(context.TODO(), req)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
@@ -104,6 +116,86 @@ func (app App) namesHandler(w http.ResponseWriter, request *http.Request) {
 
 	w.Header().Set("Content-Type", "application/xml")
 	w.Write(x)
+}
+
+func (app App) disburseHandler(w http.ResponseWriter, request *http.Request) {
+
+	var info disburseInfo
+	// Try to decode the request body into the struct. If there is an error,
+	// respond to the client with the error message and a 400 status code.
+	err := json.NewDecoder(request.Body).Decode(&info)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	refid := fmt.Sprintf("%s", strconv.FormatInt(time.Now().UnixNano(), 10))
+
+	req := ussd.A2WRequest{
+
+		Type:        "REQMFCI",
+		ReferenceID: refid,
+		Msisdn:      app.conf.AccountMSISDN,
+		PIN:         app.conf.Password,
+		Msisdn1:     info.Msisdn,
+		Amount:      info.Amount,
+		SenderName:  "Bethuel Charles",
+		Language1:   "EN",
+		BrandID:     app.conf.BrandID,
+	}
+
+
+	xmlstring, err := xml.MarshalIndent(req, "", "    ")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	xmlstring = []byte(xml.Header + string(xmlstring))
+
+	r, err := http.NewRequest("POST", app.conf.A2WReqURL, bytes.NewBuffer(xmlstring)) // URL-encoded payload
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	
+	r.Header.Add("Content-Type", "application/xml")
+	
+	client := http.Client{
+		Timeout: time.Minute,
+	}
+
+	res, err := client.Do(r)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	//log.Println(res.Status)
+	defer res.Body.Close()
+	xmlBody, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	var xmlResponse ussd.A2WResponse
+
+	err = xml.Unmarshal(xmlBody, &xmlResponse)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	x, err := xml.MarshalIndent(xmlResponse, "", "  ")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/xml")
+	w.Write(x)
+
+
+
 }
 
 func main() {

0e2f4671b297b20128e7e36fef2456f3aee22720
Pius Alfred <me.pius1102@gmail.com>
2021-05-22 11:59:28 +0300

    refactor files

--- push/client.go
+++ push/client.go
@@ -44,6 +44,9 @@ import (
 var (
 	JSONPayload PayloadType = "json"
 	XMLPayload  PayloadType = "xml"
+
+
+	_ Service = (*Client)(nil)
 )
 
 type (
@@ -94,6 +97,78 @@ type (
 		callbackHandler    CallbackResponseProvider
 	}
 )
+
+func (c *Client) BillPay(ctx context.Context, billPaymentReq BillPayRequest) (*BillPayResponse, error) {
+	var billPayResp = &BillPayResponse{}
+
+	billPaymentReq.BillerMSISDN = c.BillerMSISDN
+	req, err := c.NewRequest(http.MethodPost, c.PushPayBillRequestURL, JSONPayload, &billPaymentReq)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := c.SendWithAuth(ctx, req, billPayResp); err != nil {
+		return nil, err
+	}
+
+	return billPayResp, nil
+}
+
+func (c *Client) BillPayCallback(ctx context.Context) http.HandlerFunc {
+	var callbackRequest BillPayCallbackRequest
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+
+		err := json.NewDecoder(r.Body).Decode(&callbackRequest)
+		c.Log("Callback Request", JSONPayload, &callbackRequest)
+		defer r.Body.Close()
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		callbackResponse := c.callbackHandler(ctx, callbackRequest)
+		c.Log("Callback Response", JSONPayload, &callbackResponse)
+
+		if err := json.NewEncoder(w).Encode(callbackResponse); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+	}
+}
+
+func (c *Client) RefundPayment(ctx context.Context, refundPaymentReq RefundPaymentRequest) (*RefundPaymentResponse, error) {
+	var refundPaymentResp = &RefundPaymentResponse{}
+
+	req, err := c.NewRequest(http.MethodPost, c.PushPayReverseTransactionURL, JSONPayload, refundPaymentReq)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := c.Send(ctx, req, refundPaymentResp); err != nil {
+		return nil, err
+	}
+
+	return refundPaymentResp, nil
+}
+
+func (c *Client) HealthCheck(ctx context.Context, healthCheckReq HealthCheckRequest) (*HealthCheckResponse, error) {
+	var healthCheckResp = &HealthCheckResponse{}
+
+	req, err := c.NewRequest(http.MethodPost, c.PushPayHealthCheckURL, JSONPayload, healthCheckReq)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := c.Send(ctx, req, healthCheckResp); err != nil {
+		return nil, err
+	}
+
+	return healthCheckResp, nil
+}
 
 // NewClient initiate new tigosdk sdk used by other services.
 // Default all pretty formatted requests (in and out) and responses

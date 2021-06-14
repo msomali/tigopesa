6b7296d1931b407ae04524e4b48850515de4e6b3
Pius Alfred <me.pius1102@gmail.com>
2021-06-14 23:31:44 +0300

    refactoring

--- push/response.go
+++ push/response.go
@@ -1,24 +1,26 @@
 package push
 
-type BillPayResponse struct {
-	ResponseCode        string `json:"ResponseCode"`
-	ResponseStatus      bool   `json:"ResponseStatus"`
-	ResponseDescription string `json:"ResponseDescription"`
-	ReferenceID         string `json:"ReferenceID"`
-	Message             string `json:"Message,omitempty"`
-}
+type (
+	BillPayResponse struct {
+		ResponseCode        string `json:"ResponseCode"`
+		ResponseStatus      bool   `json:"ResponseStatus"`
+		ResponseDescription string `json:"ResponseDescription"`
+		ReferenceID         string `json:"ReferenceID"`
+		Message             string `json:"Message,omitempty"`
+	}
 
-type RefundPaymentResponse struct {
-	ResponseCode        string `json:"ResponseCode"`
-	ResponseStatus      bool   `json:"ResponseStatus"`
-	ResponseDescription string `json:"ResponseDescription"`
-	DMReferenceID       string `json:"DMReferenceID"`
-	ReferenceID         string `json:"ReferenceID"`
-	MFSTransactionID    string `json:"MFSTransactionID,omitempty"`
-	Message             string `json:"Message,omitempty"`
-}
+	RefundPaymentResponse struct {
+		ResponseCode        string `json:"ResponseCode"`
+		ResponseStatus      bool   `json:"ResponseStatus"`
+		ResponseDescription string `json:"ResponseDescription"`
+		DMReferenceID       string `json:"DMReferenceID"`
+		ReferenceID         string `json:"ReferenceID"`
+		MFSTransactionID    string `json:"MFSTransactionID,omitempty"`
+		Message             string `json:"Message,omitempty"`
+	}
 
-type HealthCheckResponse struct {
-	ReferenceID string `json:"ReferenceID"`
-	Description string `json:"Description"`
-}
+	HealthCheckResponse struct {
+		ReferenceID string `json:"ReferenceID"`
+		Description string `json:"Description"`
+	}
+)

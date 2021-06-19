46fa7884c7050748622eb3cd49f093d42c21a343
Pius Alfred <me.pius1102@gmail.com>
2021-06-19 15:24:43 +0300

    config management

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,6 +1,20 @@
 package config
 
+import (
+	"github.com/techcraftt/tigosdk/push"
+	"github.com/techcraftt/tigosdk/ussd/aw"
+	"github.com/techcraftt/tigosdk/ussd/wa"
+)
+
+var (
+	_ Validator = (*Config)(nil)
+)
+
 type (
+	
+	Config struct {
+		
+	}
 
 	//Validator validates the configurations and return nil if all
 	//is good or return an error
@@ -8,3 +22,15 @@ type (
 		Validate() error
 	}
 )
+
+func (c *Config) Validate() error {
+	panic("implement me")
+}
+
+func (c *Config) Split() (push.Config,wa.Config,aw.Config,error) {
+	return push.Config{}, wa.Config{}, aw.Config{}, nil
+}
+
+func Merge(config push.Config, config2 wa.Config, config3 aw.Config) (Config,error) {
+	return Config{}, nil
+}

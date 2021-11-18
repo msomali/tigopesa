d0a8e96529c53281d69563f3996b4741e871831b
Pius Alfred <me.pius1102@gmail.com>
2021-11-18 19:43:33 +0300

    fix: callback request float64 to string

--- push/push.go
+++ push/push.go
@@ -82,7 +82,7 @@ type (
 		Description      string `json:"Description"`
 		MFSTransactionID string `json:"MFSTransactionID,omitempty"`
 		ReferenceID      string `json:"ReferenceID"`
-		Amount           float64 `json:"Amount"`
+		Amount           string `json:"Amount"`
 	}
 
 	CallbackResponse struct {
